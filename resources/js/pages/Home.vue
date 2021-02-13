1590b1fcabdd6ea7c4fb25827d8891a9ed7a61dc
Cristian <cristian.labadie@gmail.com>
2021-02-12 21:00:22 -0300

    Dev

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,49 +1,19 @@
 <template>
-  <div class="home">
-
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
   components: {},
   props: [],
   data() {
-    return {
-      permisos: {}
-    }
+    return {};
   },
-  beforeCreate() {},
-  created() {
-    this.setPermisos()
-  },
-  beforeMount() {},
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed() {},
-  methods: {
-    setPermisos() {
-      var vm = this
-      _.each(this.$store.state.user.permisos[0].opciones, function(o) {
-        vm.$set(vm.permisos, o.nombre, o.visible)
-      })
-    }
-  }
-}
+  methods: {}
+};
 </script>
 
 <style scoped lang="scss">
 @import "~vars";
-#grafico {
-  margin-top: 20px;
-}
-
-.total_pendientes {
-  font-size: 3em;
-  text-align: center;
-}
 </style>
